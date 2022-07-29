3f00cbd2531460432214de97a431ec786f393a2d
Patrick Itorobong Okuku <patrickokuku1@gmail.com>
2022-07-29 21:18:44 +0100

    BEM design structure and Saas/Scss edits

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,32 @@
 "use-strict";
 
 //CATEGORIES CONNECTION TO SERVER
-const topics = Array.from(document.getElementsByClassName("category-topics"));
-const sectionTitle = document.getElementById("section-title");
+const categoryLinks = Array.from(
+  document.getElementsByClassName("categories__item")
+);
+const sectionTitle = document.querySelector(".news-content__title");
 
-topics.forEach((topic) => {
-  topic.addEventListener("click", (e) => {
+const categoryTile = document.querySelector(".news-content__tile");
+
+categoryLinks.forEach((link) => {
+  const originalHtml = link.innerHTML;
+  if (link.innerText === "Top Stories") {
+    const newHtml = `<a href="/" class="categories__item--link">${originalHtml}</a>`;
+    link.innerHTML = newHtml;
+  } else {
+    const newHtml = `<a href="/category" class="categories__item--link">${originalHtml}</a>`;
+    link.innerHTML = newHtml;
+  }
+});
+
+// categoryTile.addEventListener("click", (event) => {
+//   if (event.target.className === "news-content__tile--text-link") {
+//     document.querySelector("news-content__tile--text-link").click();
+//   }
+// });
+
+categoryLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
     const data = { topic: e.target.innerText };
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "/", true);
